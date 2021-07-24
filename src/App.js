e850a127cdf035443b14558e8f85bbfc9fcf5fee
zimzika <ryanbala12@gmail.com>
2021-07-23 22:42:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [display, setDisplay] = useState("");
+
+    let operators = ["*", "+", "-", "/"];
+
+    const isOperator = (char) => operators.includes(char);
+    const isNumber = (char) => !isNaN(parseInt(char));
+
+    const handleClick = (e) => {
+        let value = e.target.name;
+        let lastChar = display.slice(-1);
+
+        if (isOperator(value) && display.length <= 0) return;
+        if (isOperator(value) && display.match(/([0-9]?(\+|\*|\+|\-)[0-9])/g)) {
+            let result = eval(display);
+            return setDisplay(result.toString().concat(value));
+        }
+        if (isOperator(value) && !isNumber(lastChar)) return;
+        
+        setDisplay(display.concat(value));
+    }
+
+    const clear = () => {
+        setDisplay("");
+    }
+
+    const backspace = () => {
+        setDisplay(display.toString().slice(0, -1));
+    }
+
+    const result = () => {
+        let result = eval(display);
+        setDisplay(result);
+    }
+
+    return (
+        <>
+            <div className="container">
+                <form>
+                    <input type="text" value={display} />
+                </form>
+                <div className="keypad">
+                    {/* keypads from calculator */}
+                    <button className="highlight"onClick={clear} id="clear">Clear</button>
+                    <button className="highlight" onClick={backspace} id="backspace">C</button>
+                    <button className="highlight" name="/" onClick={handleClick}>&divide;</button>
+                    <button name="7" onClick={handleClick}>7</button>
+                    <button name="8" onClick={handleClick}>8</button>
+                    <button name="9" onClick={handleClick}>9</button>
+                    <button className="highlight" name="*" onClick={handleClick}>&times;</button>
+                    <button name="4" onClick={handleClick}>4</button>
+                    <button name="5" onClick={handleClick}>5</button>
+                    <button name="6" onClick={handleClick}>6</button>
+                    <button className="highlight" name="-" onClick={handleClick}>&ndash;</button>
+                    <button name="1" onClick={handleClick}>1</button>
+                    <button name="2" onClick={handleClick}>2</button>
+                    <button name="3" onClick={handleClick}>3</button>
+                    <button className="highlight" name="+" onClick={handleClick}>+</button>
+                    <button name="0" onClick={handleClick}>0</button>
+                    <button name="." onClick={handleClick}>.</button>
+                    <button className="highlight" onClick={result} id="result">=</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App;
